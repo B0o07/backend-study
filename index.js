e926f1b27962066e94fdddfccafb763de981ccc1
B0o07 <playssaulo@gmail.com>
2026-03-01 14:54:14 -0300

    Initial commit - JS fundamentals exercises

---
+++ index.js
@@ -0,0 +1,41 @@
+/* Primeiro Exercício
+
+function somar(primeiro, segundo) {
+    return primeiro + segundo;
+}
+
+console.log(somar(10, 20));
+
+//Segundo Exercício
+
+let user = [{usuario: "Saulo", idade: 21},{usuario: "Pedro", idade: 18},{usuario: "Ana", idade: 1},{usuario: "Tiago", idade: 22},{usuario: "Angela", idade: 3},{usuario: "Bruna", idade: 24}];
+
+const maior = user.filter(user => user.idade >= 18);
+
+console.log(maior);
+
+//Terceiro Exercício
+
+const usuario = {
+    nome: "Saulo Weslen da Silva Sousa",
+    email: "wsaullo15@gmail.com",
+};
+
+console.log(usuario);
+
+*/
+
+/* 
+
+Agora vamos subir 1 nivel
+
+Modifique a função do Primeiro Exercício para que caso um dos valores não for numero, retornar valor inválido.
+
+Dica:
+Use typeof
+
+No exercicio 2, crie uma variável que retorne apenas os NOMES dos maiores de 18
+
+Dica:
+Use filter e map
+*/
